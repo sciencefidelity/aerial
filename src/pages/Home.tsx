2981ccd775b25e21abca2ad76f8ec3d58e9f0a24
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-06 23:43:39 +0000

    feat: add intro text and main image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,9 @@
-import React, { FC, useState, useEffect } from "react"
+import React, { FC } from "react"
 import PropTypes from "prop-types"
 import type { RouteComponentProps } from "@reach/router"
 
 import Layout from "@/components/Layout"
-import { capitalize } from "@/lib/utils"
-import logo from "@/images/logo.svg"
+import site from "@/images/site.jpg"
 import styles from "@/styles/Home.module.scss"
 
 interface Props extends RouteComponentProps {
@@ -12,19 +11,16 @@ interface Props extends RouteComponentProps {
 }
 
 const Home: FC<Props> = ({ title }) => {
-  const [count, setCount] = useState(0)
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000)
-    return () => clearTimeout(timer)
-  }, [count, setCount])
   return (
     <Layout>
       <div className={styles[title]}>
-        <h1>{capitalize(title)}</h1>
-        <img src={logo} className={styles["app-logo"]} alt="logo" />
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
+        <img src={site} alt="Island Fields map" />
+        <div style={{height: "3rem"}}></div>
+        <code>
+          Climate Change in Brecknockshire results in irregular storms and significant increases in rainfall. A series of field recordings gathered throughout Autumn 2021, this work celebrates, archives and critiques the biodiversity of Brecon’s Island Fields, a flood plain situated between the River Usk and the Monmouthshire and Brecon Canal on the edge of the town. The whole ecosystem here is at risk due to increased flooding at times of the year when the nature here is vulnerable. The Island Fields is a space well loved by the local community for walking, playing, swimming and the enjoyment of the natural World.<br /><br />
+
+          Artscape artist-in-residence Matt Cook has developed this work through a series of interactions with ornithologist Andrew King; the Biodiversity Information Service for Powys and local green wood workers.
+        </code>
       </div>
     </Layout>
   )
