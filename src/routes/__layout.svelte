e0568558b34e2b2242c1765addf735c7f1a46f5d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-07 21:00:01 +0000

    refactor: consolidate css into one file

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -18,27 +18,3 @@
     </p>
   </div>
 </footer>
-
-<style>
-  header,
-  footer {
-    color: #333333;
-    width: 100vw;
-  }
-
-  header {
-    margin: auto;
-    max-width: 70rem;
-    padding: 2rem 2rem;
-  }
-
-  footer {
-    border-top: 1px solid #333333;
-  }
-
-  footer div {
-    margin: auto;
-    max-width: 70rem;
-    padding: 2rem 2rem;
-  }
-</style>
