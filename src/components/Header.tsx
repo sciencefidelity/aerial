0a41cb00ca66b9fe320c29f5e8f02467348fafc2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-07 07:47:28 +0000

    refactor: change heading size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import React, { FC } from "react"
 const Header: FC = () => {
   return (
     <header>
-      <p>
-        <strong>Brecon Sound Forage</strong>
-      </p>
+      <h1>
+        Brecon Sound Forage
+      </h1>
     </header>
   )
 }
