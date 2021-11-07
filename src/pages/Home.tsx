fa18b5c6ef9b250d172159a450b41fab499a4665
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-07 07:48:03 +0000

    feat: use custom font

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,11 +16,12 @@ const Home: FC<Props> = ({ title }) => {
       <div className={styles[title]}>
         <img src={site} alt="Island Fields map" />
         <div style={{height: "3rem"}}></div>
-        <code>
-          Climate Change in Brecknockshire results in irregular storms and significant increases in rainfall. A series of field recordings gathered throughout Autumn 2021, this work celebrates, archives and critiques the biodiversity of Brecon’s Island Fields, a flood plain situated between the River Usk and the Monmouthshire and Brecon Canal on the edge of the town. The whole ecosystem here is at risk due to increased flooding at times of the year when the nature here is vulnerable. The Island Fields is a space well loved by the local community for walking, playing, swimming and the enjoyment of the natural World.<br /><br />
-
+        <p>
+          Climate Change in Brecknockshire results in irregular storms and significant increases in rainfall. A series of field recordings gathered throughout Autumn 2021, this work celebrates, archives and critiques the biodiversity of Brecon’s Island Fields, a flood plain situated between the River Usk and the Monmouthshire and Brecon Canal on the edge of the town. The whole ecosystem here is at risk due to increased flooding at times of the year when the nature here is vulnerable. The Island Fields is a space well loved by the local community for walking, playing, swimming and the enjoyment of the natural World.
+        </p>
+        <p>
           Artscape artist-in-residence Matt Cook has developed this work through a series of interactions with ornithologist Andrew King; the Biodiversity Information Service for Powys and local green wood workers.
-        </code>
+        </p>
       </div>
     </Layout>
   )
