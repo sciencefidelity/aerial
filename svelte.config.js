577e86b21b400dbdff03624423b2afcfc2edf2da
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-08 23:09:45 +0000

    feat: set up for mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import preprocess from "svelte-preprocess"
+import { mdsvex } from "mdsvex"
+import mdsvexConfig from "./mdsvex.config.js"
 import vercel from "@sveltejs/adapter-vercel"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', ...mdsvexConfig.extensions],
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: [
+    mdsvex(mdsvexConfig),
+    preprocess({
+      postcss: true,
+      defaults: {
+        style: "scss",
+        script: "typescript"
+      }
+    })
+  ],
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
