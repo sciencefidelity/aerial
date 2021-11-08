8d2acfc30c3f9b80de0033e82c838f7c12a705f8
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-08 22:27:31 +0000

    fix: move main out of section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,30 +25,28 @@
   />
 </svelte:head>
 
-<section>
-  <main>
-    <div class="home">
-      <picture>
-        <source srcset="site.webp" type="image/webp" />
-        <img src="site.jpg" alt="Island Fields map" width="660" height="396" />
-      </picture>
-      <div style="height: 3rem" />
-      <p>
-        Climate Change in Brecknockshire results in irregular storms and
-        significant increases in rainfall. A series of field recordings gathered
-        throughout Autumn 2021, this work celebrates, archives and critiques the
-        biodiversity of Brecon’s Island Fields, a flood plain situated between
-        the River Usk and the Monmouthshire and Brecon Canal on the edge of the
-        town. The whole ecosystem here is at risk due to increased flooding at
-        times of the year when the nature here is vulnerable. The Island Fields
-        is a space well loved by the local community for walking, playing,
-        swimming and the enjoyment of the natural World.
-      </p>
-      <p>
-        Artscape artist-in-residence Matt Cook has developed this work through a
-        series of interactions with ornithologist Andrew King; the Biodiversity
-        Information Service for Powys and local green wood workers.
-      </p>
-    </div>
-  </main>
-</section>
+<main>
+  <div class="home">
+    <picture>
+      <source srcset="site.webp" type="image/webp" />
+      <img src="site.jpg" alt="Island Fields map" width="660" height="396" />
+    </picture>
+    <div style="height: 3rem" />
+    <p>
+      Climate Change in Brecknockshire results in irregular storms and
+      significant increases in rainfall. A series of field recordings gathered
+      throughout Autumn 2021, this work celebrates, archives and critiques the
+      biodiversity of Brecon’s Island Fields, a flood plain situated between
+      the River Usk and the Monmouthshire and Brecon Canal on the edge of the
+      town. The whole ecosystem here is at risk due to increased flooding at
+      times of the year when the nature here is vulnerable. The Island Fields
+      is a space well loved by the local community for walking, playing,
+      swimming and the enjoyment of the natural World.
+    </p>
+    <p>
+      Artscape artist-in-residence Matt Cook has developed this work through a
+      series of interactions with ornithologist Andrew King; the Biodiversity
+      Information Service for Powys and local green wood workers.
+    </p>
+  </div>
+</main>
