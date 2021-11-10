24de8def90cba605c42ad32035c868da583bc216
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-10 21:34:06 +0000

    feat: write script to loop a sound on the RPi pico

--- pico/code.py
+++ pico/code.py
@@ -1,14 +1,17 @@
+"""Loop and audio sample"""
 import audiocore
 import board
+import time
 import audiobusio
+import digitalio
 
 wave_file = open("StreetChicken.wav", "rb")
 wave = audiocore.WaveFile(wave_file)
 
-audio = audiobusio.I2SOut(board.D1, board.D0, board.D9)
+audio = audiobusio.I2SOut(board.GP10, board.GP11, board.GP9)
 
-while True:
-    audio.play(wave)
+audio.play(wave, loop=True)
 
 while audio.playing:
     pass
+    time.sleep(0.1)
