fc723c77d5c4e50f6ef57d761b82493de8e610f6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-10 21:33:21 +0000

    test: set up test script for Raspberry Pi Pico

---
+++ pico/test.py
@@ -0,0 +1,13 @@
+"""Test script to Flash the onboard LED"""
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.switch_to_output()
+
+while True:
+    led.value = True
+    time.sleep(1)
+    led.value = False
+    time.sleep(1)
