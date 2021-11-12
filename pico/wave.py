5ae4c162291b1fce5521676ffb785de53f8f8001
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-12 02:28:21 +0000

    feat: get the wave file to loop continuously

--- pico/wave.py
+++ pico/wave.py
@@ -1,19 +1,15 @@
-"""Play audio sample then wait 10 seconds before playing again"""
 import audiocore
 import board
 import time
 import audiobusio
 import digitalio
 
-wave_file = open("StreetChicken.wav", "rb")
+wave_file = open("sound.wav", "rb")
 wave = audiocore.WaveFile(wave_file)
 
 audio = audiobusio.I2SOut(board.GP10, board.GP11, board.GP9)
 
-while True:
-    audio.play(wave)
-    time.sleep(10)
+audio.play(wave, loop=True)
 
 while audio.playing:
     pass
-    time.sleep(0.1)
