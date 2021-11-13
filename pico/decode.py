147569a8e972d5ec1183416fc891ab01c2bc2d11
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-13 23:47:13 +0000

    feat: loop an mp3 with al

--- pico/decode.py
+++ pico/decode.py
@@ -10,8 +10,6 @@ mp3 = open("sound.mp3", "rb")
 decoder = MP3Decoder(mp3)
 audio = audiobusio.I2SOut(board.GP10, board.GP11, board.GP9)
 
-audio.play(decoder, loop=True)
-
-while audio.playing:
-    pass
-    time.sleep(0.1)
+while True:
+    audio.play(decoder)
+    time.sleep(99)
