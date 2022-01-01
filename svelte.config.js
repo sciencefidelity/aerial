35877f35735a47cd7f6e7d342624f7e45f221200
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-01 09:13:33 +0000

    build: add modules and configs for prettier, eslint and stylelint

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,16 @@
+import adapter from "@sveltejs/adapter-auto"
 import preprocess from "svelte-preprocess"
-import { mdsvex } from "mdsvex"
-import vercel from "@sveltejs/adapter-vercel"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ".md"],
-  preprocess: [
-    mdsvex({
-      extensions: [".svelte", ".md"]
-    }),
-    preprocess()
-  ],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
   kit: {
+    adapter: adapter(),
+
     // hydrate the <div id="svelte"> element in src/app.html
-    adapter: vercel(),
     target: "#svelte"
   }
 }
