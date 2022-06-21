b5fd7843f6f242a7e708b0947371a38b46360348
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-21 23:50:37 +0100

    refactor: add eslint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
+    "prettier",
+    "koa"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
