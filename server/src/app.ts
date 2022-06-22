7a0311fa317f803ea4ec9d566bf7bd5315709abf
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-22 21:15:23 +0100

    refactor: add request types

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,6 +11,20 @@ app.get("/", (_req, res) => {
   res.send("Hello World!")
 })
 
+app.post("/", (_req, res) => {
+  res.send("Got a POST request")
+})
+
+app.put("/user", (_req, res) => {
+  res.send("Got a PUT request at /user")
+})
+
+app.delete("/user", (_req, res) => {
+  res.send("Got a DELETE request at /user")
+})
+
+app.use(express.static("public"))
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
